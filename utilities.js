faaa6f6a37c6808930cff90d190d33456c8d56ef
allygilbreath <gilbrea6@msu.edu>
2025-02-18 19:54:08 +0000

    all changes

---
+++ utilities.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  
+  ScrollReveal().reveal('h1', {
+    delay: 500, 
+    origin: 'top', 
+    distance: '50px', 
+    duration: 1000 
+  });
+
+  
+  ScrollReveal().reveal('img', {
+    delay: 300, 
+    origin: 'bottom', 
+    distance: '50px', 
+    duration: 1000, 
+    interval: 300 
+  });
+};
